6ac83340e6d241514061e8d4c295aa75ef49b1b0
Ibtikar <ibtikarzofficial@gmail.com>
2026-03-25 14:00:16 +0500

    feat: WebXR AR integration

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,12 @@ import { Suspense, useState, useRef } from 'react'
 import './App.css'
 import { Canvas } from '@react-three/fiber'
 import { Environment, OrbitControls } from '@react-three/drei'
-// 1. NEW IMPORT: Grab createXRStore and XR
 import { createXRStore, XR } from '@react-three/xr'
 import gsap from 'gsap'
 import CanvasLoader from './CanvasLoader'
 import { Model as WatchModel } from './WatchModel'
 import UI from './UI'
 
-// 2. THE NEW API: Initialize the XR store outside of your component
 const store = createXRStore()
 
 function App() {
@@ -39,11 +37,6 @@ function App() {
   return (
     <div style={{ position: 'relative', height: '100%', width: '100%' }}>
 
-      {/* 3. THE TRIGGER: Standard HTML button that calls store.enterAR() */}
-
-
-      <UI setActiveColor={setActiveColor} activeColor={activeColor} viewCamera={viewCamera} />
-
       <button
         onClick={() => store.enterAR()}
         style={{
@@ -67,8 +60,9 @@ function App() {
         View in Your Space
       </button>
 
+      <UI setActiveColor={setActiveColor} activeColor={activeColor} viewCamera={viewCamera} />
+
       <Canvas camera={{ position: [0, 0, 5], fov: 50 }} gl={{ alpha: true, antialias: true }}>
-        {/* 4. THE WRAPPER: Pass the store to the XR component */}
         <XR store={store}>
           <OrbitControls
             ref={controlsRef}
@@ -77,12 +71,29 @@ function App() {
             maxPolarAngle={Math.PI / 1.5}
             enablePan={false}
           />
+
+          {/* THE AR LIFESAVER: Raw lighting that cannot fail in WebXR */}
+          <ambientLight intensity={2.5} />
+          <directionalLight position={[5, 10, 5]} intensity={4} />
+          <directionalLight position={[-5, -5, -5]} intensity={2} />
+
           <Environment preset='city' environmentIntensity={1.2} />
 
           <Suspense fallback={<CanvasLoader />}>
-            {/* THE AR FIX: Push it 0.5 meters away from the camera and scale it down */}
-            <group position={[0, 0, -0.5]} scale={[0.15, 0.15, 0.15]}>
-              <WatchModel position={[0, 0, 0]} accentColor={activeColor} />
+            {/* ANCHOR POINT: 0.5 meters exactly in front of your phone camera */}
+            <group position={[0, 0, -0.5]}>
+
+              {/* THE DEBUG CUBE: This proves if the AR positioning is working */}
+              <mesh position={[0.2, 0, 0]} scale={[0.05, 0.05, 0.05]}>
+                <boxGeometry />
+                <meshBasicMaterial color="#FF0000" />
+              </mesh>
+
+              {/* THE WATCH */}
+              <group scale={[0.15, 0.15, 0.15]}>
+                <WatchModel position={[0, 0, 0]} accentColor={activeColor} />
+              </group>
+
             </group>
           </Suspense>
         </XR>
